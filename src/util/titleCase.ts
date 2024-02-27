4db7ae806d7d2581af4c378083f0362cea456b92
Vince Chapman <vchapandrews@gmail.com>
2024-02-27 15:06:45 +0000

    Adjust to factor in undefined string param

--- src/util/titleCase.ts
+++ src/util/titleCase.ts
@@ -1,3 +1,7 @@
-export function toTitleCase(string: string) {
-    return string[0].toUpperCase() + string.slice(1);
+export function toTitleCase(string: string | undefined) {
+    if (!string) {
+        return string;
+    } else {
+        return string[0].toUpperCase() + string.slice(1);
+    }
 }
